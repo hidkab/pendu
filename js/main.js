0c98df69ad68dec627e483a47509a4a28429fc49
Hidouche AIT YOUCEF <hidouche.aityoucef@gmail.com>
2021-03-07 19:42:04 +0100

    Global variables

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+console.log(" hang game");
+// ************** Global variables *****************
+
+let gameWords = ["manger", "tomate", "variable", "jeu", "pendu"];
+let guessWords = ["------", "------", "--------", "---", "----"];
+
+// ************** Function ******************
